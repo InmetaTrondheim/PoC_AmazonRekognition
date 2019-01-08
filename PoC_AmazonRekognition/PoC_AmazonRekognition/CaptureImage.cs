c131ebcc5dbde9a59499081061b5cdf89d6acb01
TordStavik <tord.stavik@inmeta.no>
2019-01-08 16:21:55 +0100

    Added camera option

---
+++ PoC_AmazonRekognition/PoC_AmazonRekognition/CaptureImage.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using AForge.Video;
+using AForge.Video.DirectShow;
+
+namespace PoC_AmazonRekognition
+{
+    public static class CaptureImage
+    {
+        private static FilterInfoCollection CaptureDevice;
+        private static VideoCaptureDevice FinalFrame;
+        private static int selectedDevice;
+        private static bool takePicture = false;
+        private static Stopwatch stopwatch = new Stopwatch();
+
+        public static void FindCapturingDevice()
+        {
+            CaptureDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (CaptureDevice.Count > 0)
+            {
+                FinalFrame = new VideoCaptureDevice();
+                SelectCapturingDevice();
+            }
+            else
+            {
+                Console.WriteLine("You do not have a device for video capture");
+            }
+        }
+
+        public static void SelectCapturingDevice()
+        {
+            if (CaptureDevice.Count == 1)
+            {
+                selectedDevice = 1;
+                Console.WriteLine("\nUsing " + CaptureDevice[0].Name + "for capturing image");
+                SaveImage();
+            }
+            else
+            {
+                Console.WriteLine("\nPlease select your desired camera\n");
+
+                int count = 1;
+                foreach (FilterInfo cam in CaptureDevice)
+                {
+                    Console.WriteLine($"{count}. {cam.Name}");
+                    count++;
+                }
+
+                char userInput = Console.ReadKey().KeyChar;
+
+                if (char.IsDigit(userInput))
+                {
+                    selectedDevice = (int)char.GetNumericValue(userInput);
+                    SaveImage();
+                }
+            }
+        }
+
+        private static void SaveImage()
+        {
+            FinalFrame = new VideoCaptureDevice(CaptureDevice[selectedDevice - 1].MonikerString);
+            FinalFrame.NewFrame += new NewFrameEventHandler(FinalFrame_NewFrame);
+            FinalFrame.Start();
+        }
+
+        private static async void FinalFrame_NewFrame(object sender, NewFrameEventArgs eventArgs)
+        {
+            var image = (Bitmap)eventArgs.Frame.Clone();
+            Bitmap varBmp = new Bitmap(image);
+
+            varBmp.Save(GetNextFileName(), ImageFormat.Png);
+            Console.WriteLine($"\nSaving photo");
+            varBmp.Dispose();
+            varBmp = null;
+            stopwatch.Restart();
+            var dir = GetDirectory();
+            DirectoryInfo directory = new DirectoryInfo(dir);
+            var filePairs = directory
+                .GetFiles("*.png")
+                .OrderByDescending(f => f.CreationTime);
+            Program.DoImageRecognition(filePairs.First().FullName);
+
+            FinalFrame.SignalToStop();
+        }
+
+        private static string GetNextFileName()
+        {
+            string path = GetDirectory();
+            bool exists = System.IO.Directory.Exists(path);
+
+            if (!exists)
+            {
+                var directory = Directory.CreateDirectory(path);
+            }
+            int fCount = Directory.GetFiles(path, "*", SearchOption.AllDirectories).Length;
+
+            var imgPath = path + @"\capture" + fCount + ".png";
+            return imgPath;
+        }
+
+        private static string GetDirectory()
+        {
+            return System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location) + @"\images";
+        }
+    }
+}
--- PoC_AmazonRekognition/PoC_AmazonRekognition/Program.cs
+++ PoC_AmazonRekognition/PoC_AmazonRekognition/Program.cs
@@ -19,72 +19,105 @@ namespace PoC_AmazonRekognition
         private static bool foundSerial = false;
         private static string serialNumber = "";
         private static Stopwatch _stopwatch = new Stopwatch();
+        private static bool again = true;
 
         static void Main(string[] args)
         {
-            string fullPath = GetDirectory();
-            Console.WriteLine($"Please select image 1-9");
+            Console.WriteLine("Press 0 for image processing using camera or 1-9 for stored images");
+
+            while (again)
+            {
+                foundSerial = false;
+                serialNumber = string.Empty;
+                PresentMenu();
+            }
+        }
+
+        private static void PresentMenu()
+        {
             char userInput = Console.ReadKey().KeyChar;
             Console.WriteLine();
             if (char.IsDigit(userInput))
             {
-                string path = fullPath + "\\images\\" + userInput + ".jpg";
-                if (File.Exists(path))
+                if (userInput == '0')
                 {
-                    var options = new CredentialProfileOptions()
-                    {
-                        AccessKey = "<INSERT ACCESS KEY",
-                        SecretKey = "<INSERT SECRET KEY"
-                    };
-
-                    var profile = new CredentialProfile("test", options) { Region = RegionEndpoint.USEast2 };
-                    var netSdkFile = new NetSDKCredentialsFile();
-                    netSdkFile.RegisterProfile(profile);
-
-
-                    AmazonRekognitionClient rekoClient = new AmazonRekognitionClient(RegionEndpoint.USEast2);
-                    MemoryStream mStream;
-
-                    using (System.Drawing.Image image = System.Drawing.Image.FromFile($"{fullPath}\\images\\9.jpg"))
-                    {
-                        using (MemoryStream m = new MemoryStream())
-                        {
-                            image.Save(m, image.RawFormat);
-                            mStream = m;
-                        }
-                    }
-
-                    DetectTextRequest detectTextRequest = new DetectTextRequest()
-                    {
-                        Image = new Image()
-                        {
-                            Bytes = mStream
-                        }
-                    };
-
-                    try
-                    {
-                        _stopwatch.Start();
-                        DetectTextResponse detectTextResponse = rekoClient.DetectText(detectTextRequest);
-                        foreach (TextDetection text in detectTextResponse.TextDetections)
-                        {
-                            CheckFoundSerial(text.DetectedText);
-                        }
-
-                        Console.WriteLine($"Query time {_stopwatch.ElapsedMilliseconds}ms");
-                        if (foundSerial)
-                        {
-                            Console.WriteLine($"Serial number: {serialNumber}");
-                        }
-                    }
-                    catch (Exception e)
-                    {
-                        Console.WriteLine(e.Message);
-                    }
+                    CaptureImage.FindCapturingDevice();
+                }
+                else
+                {
+                    string path = GetDirectory() + "\\images\\" + userInput + ".jpg";
+                    DoImageRecognition(path);
                 }
             }
+        }
 
+        public static void DoImageRecognition(string path)
+        {
+            if (File.Exists(path))
+            {
+                var options = new CredentialProfileOptions()
+                {
+                    AccessKey = "",
+                    SecretKey = ""
+                };
+
+                var profile = new CredentialProfile("test", options) { Region = RegionEndpoint.USEast2 };
+                var netSdkFile = new NetSDKCredentialsFile();
+                netSdkFile.RegisterProfile(profile);
+
+
+                AmazonRekognitionClient rekoClient = new AmazonRekognitionClient(RegionEndpoint.USEast2);
+                MemoryStream mStream;
+
+                using (System.Drawing.Image image = System.Drawing.Image.FromFile($"{path}"))
+                {
+                    using (MemoryStream m = new MemoryStream())
+                    {
+                        image.Save(m, image.RawFormat);
+                        mStream = m;
+                    }
+                }
+
+                DetectTextRequest detectTextRequest = new DetectTextRequest()
+                {
+                    Image = new Image()
+                    {
+                        Bytes = mStream
+                    }
+                };
+
+                try
+                {
+                    _stopwatch.Start();
+                    DetectTextResponse detectTextResponse = rekoClient.DetectText(detectTextRequest);
+                    foreach (TextDetection text in detectTextResponse.TextDetections)
+                    {
+                        CheckFoundSerial(text.DetectedText);
+                    }
+
+                    Console.WriteLine($"Query time {_stopwatch.ElapsedMilliseconds}ms");
+                    if (foundSerial)
+                    {
+                        Console.WriteLine($"Serial number: {serialNumber}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Could not find any serial number");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+               
+            _stopwatch.Reset();
+            Console.WriteLine();
+            Console.WriteLine("Press enter to show menu");
             Console.ReadKey();
+            Console.Clear();
+            Console.WriteLine("Press 0 for image processing using camera or 1-9 for stored images");
+
         }
 
         private static void CheckFoundSerial(string text)
